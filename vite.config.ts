c678041cc803ccc5fcae3805cc57980901d079e8
Jayden Calhoun <jaydencalhoun@Jaydens-MacBook-Air.local>
2025-06-19 13:27:13 -0400

    Final cleanup for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  root: '.', // (default — make sure you're not pointing to /public)
+  build: {
+    outDir: 'dist',
+  },
+})
+
