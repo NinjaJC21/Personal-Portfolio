742bb3a6e56ba98c68ef2d2466da5856ad6ee39d
Jayden Calhoun <jaydencalhoun@Jaydens-MacBook-Air.local>
2025-06-19 05:18:22 -0400

    Removed duplicate js files to fix ts setup

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment, jsxs as _jsxs } from "react/jsx-runtime";
-// src/App.tsx
-import './App.css';
-import Contact from './sections/contact/Contact';
-import About from './sections/about/About';
-import Portfolio from './sections/portfolio/Portfolio';
-export default function App() {
-    return (_jsxs(_Fragment, { children: [_jsx(About, {}), _jsx(Portfolio, {}), _jsx(Contact, {})] }));
-}
